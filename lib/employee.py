7cf8be2fdc856640a1ab8bb6876b9d6a2076d0e7
Ruth <ruro122020@gmail.com>
2023-12-09 11:25:26 -0500

    reviews method in Employee class done && lab complete

--- lib/employee.py
+++ lib/employee.py
@@ -186,5 +186,13 @@ class Employee:
         return cls.instance_from_db(row) if row else None
 
     def reviews(self):
+        from review import Review
         """Return list of reviews associated with current employee"""
-        pass
+        sql = """
+            SELECT * FROM reviews
+            WHERE employee_id = ?
+        """
+        CURSOR.execute(sql, (self.id,),)
+        rows = CURSOR.fetchall()
+        
+        return [Review.instance_from_db(row) for row in rows]
